9793519b155693713b3f884437ad28782526e7d4
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2018-11-22 15:28:20 +0100

    Added Demo parameter Initial commit

---
+++ 000hexya.go
@@ -0,0 +1,31 @@
+// Copyright 2017 NDP Systèmes. All Rights Reserved.
+// See LICENSE file for full licensing details.
+
+package decimalPrecision
+
+import (
+	// decimalPrecision depends on base module
+	_ "github.com/hexya-addons/base"
+	"github.com/hexya-erp/hexya/src/server"
+	"github.com/hexya-erp/hexya/src/tools/nbutils"
+)
+
+const MODULE_NAME string = "decimalPrecision"
+
+var Precisions = map[string]nbutils.Digits{}
+
+func init() {
+	server.RegisterModule(&server.Module{
+		Name:     MODULE_NAME,
+		PostInit: func() {},
+	})
+}
+
+// GetPrecision returns the precision for the given application
+func GetPrecision(app string) nbutils.Digits {
+	res, exists := Precisions[app]
+	if !exists {
+		return nbutils.Digits{Precision: 16, Scale: 2}
+	}
+	return res
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/hexya-addons/decimalPrecision
+
+require (
+	github.com/hexya-addons/base v0.0.0-20181122141345-d966bf34a56f
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/spf13/cast v1.3.0 // indirect
+	github.com/spf13/pflag v1.0.3 // indirect
+	golang.org/x/sys v0.0.0-20181122145206-62eef0e2fa9b // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
